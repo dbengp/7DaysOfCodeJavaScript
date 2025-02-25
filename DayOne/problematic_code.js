3fd1170b0a53323cadbcb925de05d26cd14648d0
daelton pinheiro <167815532+dbengp@users.noreply.github.com>
2025-02-25 11:36:40 -0300

    Delete DayOne/problematic_code.js

--- DayOne/problematic_code.js
+++
@@ -1,8 +0,0 @@
-let idadeSegurado = '25'; // Idade armazenada como string
-let idadeMinima = 18; // Idade mínima para o seguro
-
-if (idadeSegurado == idadeMinima) {
-  console.log('O segurado atende à idade mínima.');
-} else {
-  console.log('O segurado não atende à idade mínima.');
-}
