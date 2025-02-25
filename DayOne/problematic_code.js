0a53b0a5b8d34cc1c2b60c54401c07475ca48551
daelton pinheiro <167815532+dbengp@users.noreply.github.com>
2025-02-25 10:53:55 -0300

    Create problematic_code.js

---
+++ DayOne/problematic_code.js
@@ -0,0 +1,8 @@
+let idadeSegurado = '25'; // Idade armazenada como string
+let idadeMinima = 18; // Idade mínima para o seguro
+
+if (idadeSegurado == idadeMinima) {
+  console.log('O segurado atende à idade mínima.');
+} else {
+  console.log('O segurado não atende à idade mínima.');
+}
